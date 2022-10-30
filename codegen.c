999ee903118af73bb78f25ab61fb05b394ce0ad7
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2022-10-30 19:37:38 +1100

    Generate code for binary expressions

--- codegen.c
+++ codegen.c
@@ -1,6 +1,26 @@
 #include "minic.h"
 
+void GenExpression(struct expression Expression);
+
 global_variable struct func *CurrentFunction;
+global_variable usize Depth;
+
+static void
+Push(void)
+{
+	printf("\tsub\tsp, sp, #16\n");
+	printf("\tstr\tx8, [sp]\n");
+	Depth++;
+}
+
+static void
+Pop(char *arg)
+{
+	printf("\tldr\t%s, [sp]\n", arg);
+	printf("\tadd\tsp, sp, #16\n");
+	Depth--;
+}
+
 
 void
 GenAddress(struct expression Expression)
@@ -9,27 +29,91 @@ GenAddress(struct expression Expression)
 }
 
 void
+GenBinaryExpression(struct expression Expression)
+{
+	GenExpression(*Expression.Rhs);
+	Push();
+	GenExpression(*Expression.Lhs);
+	Pop("x9");
+
+	switch (Expression.Operator) {
+	case OP_ADD:
+		printf("\tadd\tx8, x8, x9\n");
+		break;
+
+	case OP_SUBTRACT:
+		printf("\tsub\tx8, x8, x9\n");
+		break;
+
+	case OP_MULTIPLY:
+		printf("\tmul\tx8, x8, x9\n");
+		break;
+
+	case OP_DIVIDE:
+		printf("\tsdiv\tx8, x8, x9\n");
+		break;
+
+	case OP_AND:
+		printf("\tand\tx8, x8, x9\n");
+		break;
+
+	case OP_OR:
+		printf("\torr\tx8, x8, x9\n");
+		break;
+
+	case OP_EQUAL:
+		printf("\tcmp\tx8, x9\n");
+		printf("\tcset\tx8, eq\n");
+		break;
+
+	case OP_NOT_EQUAL:
+		printf("\tcmp\tx8, x9\n");
+		printf("\tcset\tx8, ne\n");
+		break;
+
+	case OP_LESS_THAN:
+		printf("\tcmp\tx8, x9\n");
+		printf("\tcset\tx8, lt\n");
+		break;
+
+	case OP_LESS_THAN_EQUAL:
+		printf("\tcmp\tx8, x9\n");
+		printf("\tcset\tx8, le\n");
+		break;
+	}
+}
+
+void
 GenExpression(struct expression Expression)
 {
 	switch (Expression.Kind) {
 	case EK_NUMBER:
 		printf("\tmov\tx8, #%zu\n", Expression.Value);
-		return;
+		break;
+
 	case EK_VARIABLE:
 		printf("\tldr\tx8, [x29, #%zu]\n", Expression.Local->Offset);
-		return;
+		break;
+
 	case EK_CALL:
 		abort();
+
 	case EK_BINARY:
-		abort();
+		GenBinaryExpression(Expression);
+		break;
+
 	case EK_NOT:
 		abort();
+		break;
+
 	case EK_ADDRESS_OF:
 		GenAddress(Expression);
-		return;
+		break;
+
 	case EK_DEREFERENCE:
 		GenAddress(Expression);
 		printf("\tldr\tx8, [x8]\n");
+		break;
 	}
 }
 
@@ -39,18 +123,22 @@ GenStatement(struct statement Statement)
 	switch (Statement.Kind) {
 	case SK_VAR:
 		// ST_VAR actually serves no purpose at the moment
-		return;
+		break;
+
 	case SK_BLOCK:
 		for (usize I = 0; I < Statement.NumStatements; I++)
 			GenStatement(Statement.Statements[I]);
-		return;
+		break;
+
 	case SK_EXPRESSION:
 		GenExpression(Statement.Expression);
-		return;
+		break;
+
 	case SK_RETURN:
 		GenExpression(Statement.Expression);
 		printf("\tmov\tx0, x8\n");
 		printf("\tb\t.L.return.%s\n", CurrentFunction->Name);
+		break;
 	}
 }
 
@@ -116,6 +204,7 @@ GenFunction(struct func *Function)
 	GenPrologue();
 
 	GenStatement(Function->Body);
+	Assert(Depth == 0); // all Push and Pop calls must be matched
 
 	printf(".L.return.%s:\n", Function->Name);
 	GenEpilogue();
