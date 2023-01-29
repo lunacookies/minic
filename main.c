ccbdb9892b9f00e82d93236e6a986d9c5870b6fc
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-01-29 18:35:15 +1100

    Stop printing the name and content of every file

--- main.c
+++ main.c
@@ -7,12 +7,8 @@ int main()
 
 	projectSpec current_project = discoverProject(&m);
 	setCurrentProject(current_project);
-
 	for (u16 i = 0; i < current_project.num_files; i++) {
 		setCurrentFile(i);
-
-		printf("name: %s\n", current_project.file_names[i]);
-		printf("content: %s", current_project.file_contents[i]);
 
 		u8 *content = current_project.file_contents[i];
 		tokenBuffer tokens = lex(content, &m);
